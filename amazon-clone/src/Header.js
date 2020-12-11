77ac9a53ade0b39de783d58ffd21d0546f322955
Maharsh <mgheewala@hawk.iit.edu>
2020-12-10 21:26:52 -0600

    Header Done

--- amazon-clone/src/Header.js
+++ amazon-clone/src/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 
 function Header() {
     return (
         <div className="header">
             <img
-                src="https://www.freelogodesign.org/file/app/blog/20180911090509731amazon_logo_RGB.jpg"
+                className="header__logo"
+                src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
                 alt="" />
             <div className="header__search">
                 <input
                     className="header__searchInput" type="text" />
+                <SearchIcon
+                    className="header__searchIcon" />
             </div>
             <div className="header__nav">
                 <div className="header__option">
@@ -33,6 +38,11 @@ function Header() {
                 <div className="header__option">
                     <span
                         className="header__optionLineOne"></span>
+                </div>
+                <div className="header__optionBasker">
+                    <ShoppingBasketIcon />
+                    <span
+                        className="header__optionLineTwo header__basketCount">0</span>
                 </div>
             </div>
         </div>
